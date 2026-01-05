98170df13acbbad47d9d40f3dab201dc1f820882
Người hướng dẫn <longtho638@gmail.com>
2026-01-05 23:14:48 +0700

    fix: remove next-intl/plugin wrapper to fix Edge middleware __dirname error
    The next-intl/plugin was bundling Node.js dependencies into the middleware
    which caused __dirname is not defined error on Vercel Edge Runtime.
    Removing the plugin wrapper should allow middleware to run in Edge.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
-const createNextIntlPlugin = require('next-intl/plugin');
-
-const withNextIntl = createNextIntlPlugin();
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: true,
     },
-    // Note: webpack fallbacks removed - middleware runs in Edge Runtime
-    // which doesn't support Node.js modules
 }
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
